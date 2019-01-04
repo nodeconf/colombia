d5eaf4bba41a10a7a02f40ff28320207046c18a7
maleja111 <m.aleja3000@gmail.com>
2019-01-03 21:26:18 -0500

    Hotfix/cfp info (#19)
    * Update Image CFP
    
    * Update Slider Transition Timer Increaser
    
    * Update Tierney Bio

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,19 +1,19 @@
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:0,
-    autoplay:true,
-    autoplayTimeout:3000,
-    nav:true,
-    navText:['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-     responsive:{
-        0:{
-            items:1
+    loop: true,
+    margin: 0,
+    autoplay: true,
+    autoplayTimeout: 11000,
+    nav: true,
+    navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:1
+        600: {
+            items: 1
         },
-        1000:{
-            items:1
+        1000: {
+            items: 1
         }
     }
 })
