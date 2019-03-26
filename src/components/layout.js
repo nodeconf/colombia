ac7af911be4ef09cd3168b1698efb81c40c7c45b
jvelezpo <sebasvelez@gmail.com>
2019-03-26 14:03:14 -0500

    footer component

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
+import Footer from './footer'
 import SEO from './seo'
 import './layout.scss'
 
@@ -30,12 +31,8 @@ const Layout = ({ children }) => (
           }}
         >
           {children}
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
+          </div>
+        <Footer />
       </>
     )}
   />
