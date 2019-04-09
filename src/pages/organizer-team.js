2f7ab919824ddb4bfaf79b2525837762d3994d80
Milton-code <miltoncode@gmail.com>
2019-04-08 21:55:43 -0500

    Page organizer team - Ready Test

--- src/pages/organizer-team.js
+++ src/pages/organizer-team.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import { Link } from 'gatsby'
+//import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-
+import Team from '../components/team'
 const SecondPage = () => (
   <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <Team/>
   </Layout>
 )
 
