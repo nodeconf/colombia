5f0758467cedacb5b703336d325c5e2be9c18fcd
Milton Code <37517478+Milton-code@users.noreply.github.com>
2019-04-09 19:08:26 -0500

    cambios en la sesion - VENUE (#37)
    Se agrega la imagen de hotel 10
    se agrega el link, para las reservas
    se agrega el link en las imagenes de hotel10 y rutan

--- src/components/venue/index.js
+++ src/components/venue/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import Rutan from '../images/image-hotel'
 import VenueImg from '../images/image-venue-image'
+import Hotel10 from '../images/image-10hotels'
 
 import './index.scss';
 
@@ -13,16 +14,33 @@ export default class Venue extends Component{
             <h2 className="Venue-title">VENUE</h2>
             <p>Hotels we</p>
             <p>recommend </p>
-            <a href="#!" className="Venue-btn">More...</a>
+            <div className="Venue-hotels10">
+                <a 
+                    href="https://www.diezhotel.com/"
+                    target="__blank"
+                >
+                    <Hotel10 />
+                </a>
+            </div>
+                <a 
+                href="https://reservations.travelclick.com/103293?groupID=2323678" 
+                className="Venue-btn"
+                target="__blank"
+                >Reservation...</a>
             </div>
             <div className="Venue-img">
                 <VenueImg  />
             </div>
             <div className="Venue-text">
                 <div className="Venue-rutan">
-                    <div className="logo">
-                    <Rutan alt="Logo-rutan"/>
-                    </div>
+                    <a 
+                        href="https://www.rutanmedellin.org/es/"
+                        target="__blank"
+                        >
+                        <div className="logo">
+                            <Rutan alt="Logo-rutan"/>
+                        </div>
+                    </a>
                 <p>Complejo Ruta N Calle 67 N° 52-20</p>
                 <p>Medellin - Colombia. </p>
                 <p className="span-venue">Strategic location, whit easy access  to all of </p> 
