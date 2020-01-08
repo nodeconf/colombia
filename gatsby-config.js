08f2fc2608902087acf24c8cddd92fea008d1fcf
jvelezpo <sebasvelez@gmail.com>
2020-01-07 19:04:10 -0500

    fix dates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `NodeConf Colombia 2020 | July 10th and 1th, 2020 - Medellín`,
+    title: `NodeConf Colombia 2020 | July 10th and 11th, 2020 - Medellín`,
     titleShort: `NodeConf Colombia 2020`,
     description: `NodeConf Colombia 2020 is the first international event focused on the entire Node.js ecosystem.
     It’s a non-profit event, where our attendees will be sharing in an environment of inclusion and respect, having access to relevant information through talks, workshops, and great experiences with the Colombian Node community.`,
