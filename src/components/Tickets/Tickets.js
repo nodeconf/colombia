dd9a7add004113bb6756cdbb2f988414aaa68976
Stiwar Asprilla <40145112+Stiwar9816@users.noreply.github.com>
2020-01-15 14:30:12 -0500

    Section tickets, menu fixed (#51)

--- src/components/Tickets/Tickets.js
+++ src/components/Tickets/Tickets.js
@@ -4,16 +4,29 @@ import "./Tickets.scss"
 
 const Tickets = () => (
   <section className="section-tickets  l-container  l-inner-p" id="tickets">
-    <h2 className="section-cfp__title  section__title">Tickets</h2>
-    <p className="section-tickets__subtitle">
-      Early Bird tickets $100
-      <a
-        href="https://tickets.nodeconf.co/" 
-        target="_blank" 
-        className="submit"
-        rel="noopener noreferrer"
-      >Buy</a>
-    </p>
+    <div className="section-tickets__data">
+      <h2 className="section-cfp__title  section__title">Tickets</h2>
+      <h3 className="section-tickets__subtittle">We want to meet you!</h3>
+    </div>
+
+    <div className="inner-wrap-desktop">
+      <div className="ticket-soul-out">
+        <div className="section-tickets__content-ticket">
+          <div className="section-tickets__out-ticket" target="_blank">
+            <div className="section-tickets__info">Early Bird Tickets</div>
+            <div className="section-tickets__price">$ 100</div>
+            <a
+              href="https://tickets.nodeconf.co/"
+              target="_blank"
+              className="submit"
+              rel="noopener noreferrer"
+            >
+              Buy
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 )
 
